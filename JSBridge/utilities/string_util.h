e63f05bcbcd5a7379a172eec1fccdbbc99cf363b
sam.wang <san.wang@abc360.com>
2019-02-28 12:20:35 +0800

    delete

--- JSBridge/utilities/string_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRING_UTIL_H
-#define STRING_UTIL_H
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> SplitStr(const std::string &content, char c, bool skipNull);
-
-#endif
